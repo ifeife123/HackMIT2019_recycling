06ad55791e7d48de383857d8c6bf4738b7af90ef
Ife Ademolu-Odeneye <ife.ademoluodeneye@gmail.com>
2019-09-15 10:13:03 -0400

    renaming files
    main.py moved main_old.py
    main2.py is now main.py

--- main.py
+++ main.py
@@ -7,29 +7,41 @@ import time
 from recycling_status.determinator import check_list
 import cv2
 
+cap = cv2.VideoCapture(0)
+imageIndex = 0
 while True:
-    wb = webcam.WebCam()
-    labels = []
-    objects = []
-    # for filename in wb.imageFileQueue:
-    filename = wb.imageFileQueue[-1]
-    print(filename)
-    labels.extend(image_processing.get_labels(filename))
-    wb.clear_queue()
-    igen_recycle = check_list(labels)
-    print(labels)
-    print(igen_recycle)
-    '''
-    Send the label values somewhere to be processed
-    and get a string in return: ("recycle", "compostable", or "trash")
-    and display the results in a window
-    '''
-    if igen_recycle == "recycle":
-        wb.display_result("result_img/recycle.png")
-    elif igen_recycle == "not_recycle":
-        wb.display_result("result_img/no_recycle.png")
-    elif igen_recycle == "dont_know":
-        new_label = wb.display_result("result_img/unknown.png")
+    imageIndex += 1
+    success, frame = cap.read()
+    cv2.imshow("okay", frame)
+    k = cv2.waitKey(10)
+    if k == ord('q'):
+        cv2.destroyAllWindows()
+        break
+    if k == ord('s'):
+        cv2.imwrite("frames/frame%d.jpg" % imageIndex, frame)
 
-    time.sleep(1)
-    # wb.start_recording()
+        labels = []
+        objects = []
+        filename = "frames/frame%d.jpg" % imageIndex
+        labels.extend(image_processing.get_labels(filename))
+        igen_recycle = check_list(labels)
+        print(labels)
+        print(igen_recycle)
+        if igen_recycle == "recycle":
+            cv2.imshow("out",cv2.imread("result_img/recycle.png"))
+        elif igen_recycle == "not_recycle":
+            cv2.imshow("out",cv2.imread("result_img/no_recycle.png"))
+        elif igen_recycle == "dont_know":
+            cv2.imshow("out",cv2.imread("result_img/unknown.png"))
+
+            l = cv2.waitKey(0)
+
+            is_recycled = 1 if l == ord('y') else 0
+            rows = zip(labels, [is_recycled for i in range(len(labels))])
+
+            with open('record.csv', 'a') as csvFile:
+                writer = csv.writer(csvFile)
+                for row in rows:
+                    writer.writerow(row)
+            csvFile.close()
+        # cv2.waitKey(100)
---
+++ main_old.py
@@ -0,0 +1,35 @@
+'''
+main executable
+'''
+
+from image_detection import webcam, image_processing
+import time
+from recycling_status.determinator import check_list
+import cv2
+
+while True:
+    wb = webcam.WebCam()
+    labels = []
+    objects = []
+    # for filename in wb.imageFileQueue:
+    filename = wb.imageFileQueue[-1]
+    print(filename)
+    labels.extend(image_processing.get_labels(filename))
+    wb.clear_queue()
+    igen_recycle = check_list(labels)
+    print(labels)
+    print(igen_recycle)
+    '''
+    Send the label values somewhere to be processed
+    and get a string in return: ("recycle", "compostable", or "trash")
+    and display the results in a window
+    '''
+    if igen_recycle == "recycle":
+        wb.display_result("result_img/recycle.png")
+    elif igen_recycle == "not_recycle":
+        wb.display_result("result_img/no_recycle.png")
+    elif igen_recycle == "dont_know":
+        new_label = wb.display_result("result_img/unknown.png")
+
+    time.sleep(1)
+    # wb.start_recording()
